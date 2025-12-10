63155d83489bfbaf3ef21457806486deecf4fafb
Reshmajos <reshmajose1808@gmail.com>
2025-12-10 09:42:48 +0530

    ADD : google sign in

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const users = require('../models/userModel')
+// jsonwebtoken
+const jwt = require('jsonwebtoken')
 
 // register api request
 exports.registerController = async (req,res)=>{
@@ -28,5 +30,62 @@ exports.registerController = async (req,res)=>{
 }
 
 // login api
+exports.loginController = async (req,res)=>{
+    console.log("inside registercontroller");
+    const {email,password} = req.body
+    console.log(email,password);
+
+    try{
+        // check mail in model
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            if(password==existingUser.password){
+                // generate token
+                const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWTSECRET)
+            res.status(200).json({user:existingUser,token})
+            }else{
+            res.status(401).json("Incorrect Email / Password")
+        }}else{
+            res.status(404).json("Account doesnot exists...")
+        }
+    }catch(error){
+        console.log(error);
+      res.status(500).json(error)
+    }
+    // console.log(req.body);
+    
+}
+
+// google login
+
+exports.googleLoginController = async (req,res)=>{
+    console.log("inside registercontroller");
+    const {email,password,username,picture} = req.body
+    console.log(email,password,username,picture);
+
+    try{
+        // check mail in model
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            // login
+            // generate token
+                const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWTSECRET)
+            res.status(200).json({user:existingUser,token})   
+            }else{
+            //  register
+            const newuser = await users.create({
+                username,email,password,picture
+            })
+            // generate token
+                const token = jwt.sign({userMail:newuser.email,role:newuser.role},process.env.JWTSECRET)
+            res.status(200).json({user:existingUser,token})
+        }
+    }catch(error){
+        console.log(error);
+      res.status(500).json(error)
+    }
+    // console.log(req.body);
+    
+}
 // user edit profile
 // admin edit profile
--- routes/routing.js
+++ routes/routing.js
@@ -8,5 +8,9 @@ const router = new express.Router()
 // define path for client api request
 // register
 router.post('/register',userController.registerController)
+// login
+router.post('/login',userController.loginController)
+// google login
+router.post('/google/sign-in',userController.googleLoginController)
 
 module.exports = router
